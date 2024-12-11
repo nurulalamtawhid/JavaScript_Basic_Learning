5284e9f56507d488eec0f851269601fbefbea930
unknown <tawhidalam69@gmail.com>
2024-12-12 03:20:04 +0600

    variable

--- Day1.js
+++ Day1.js
@@ -1,1 +1,16 @@
-console.log("Lets start")
+/*variable*/
+
+const mobileNumber = 02345;/*not changeable for further use once assigned*/
+let email = "absds@hmail.com"
+/* let & var are quite same(both can change the value for further use) but
+   but before modern days in js, var has some issues on scoping,
+   once the value change its changes all of its scope(functional scope)
+   &(block scope). So we should use let insted of var to avoid scoping issues.
+   
+   
+   after decalre a variable if we couldnot assigned its value ,the value will 
+   remain undefined untill its defined a value.
+
+
+*/
+console.table([mobileNumber,email])/* learning some diiferent use of console */
