22f19ad62d22302c936352c95b3caf39742189e9
unknown <tawhidalam69@gmail.com>
2024-12-14 01:47:49 +0600

    data types

---
+++ Day2.js
@@ -0,0 +1,28 @@
+/* DATA Types */
+
+/* 
+Js has 2 types of Data
+
+    Js case-sensitive
+
+1. Primitive Type- is defined by JS .(js owned datatype)
+        => Numbers(0,1,1.5,2.9,-9)
+        =>Strings('hurrah')
+        =>Boolean(True & False)
+        =>Null(sometimes we have to store at least something we use null as a something
+                Its a special type of data used as a absence of an object )
+        =>Undefined(A variable which is declared but not stored gives us undefined)
+2.Object Type(Non primitive type)- user defined data type creates from primitive data type
+        => Array
+        =>Object
+        => Function
+*/
+var Nomber = 23.4;
+
+const name = 'Tawhid';
+
+let x= true;
+let y = false;
+let c = null;
+let d;/* gives undefined result */
+console.log(x)
