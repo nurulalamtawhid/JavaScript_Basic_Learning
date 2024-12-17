f62a7146af0c7e39a5f730b09a3ba04005fe3e1a
unknown <tawhidalam69@gmail.com>
2024-12-17 21:09:46 +0600

    ternary operator

---
+++ Day3.js
@@ -0,0 +1,14 @@
+//Ternary operator how it works
+
+
+/*ternary operator is a short form of if else condition.
+
+   var x = condition ? 'expressionIfTrue' : 'expressionIfFalse'
+   
+
+
+*/
+
+let number = 5;
+const result =  number%2===0? 'even': 'Odd';/* if(number%2===0){console.log('even')}else{console.log('odd')} */
+console.log(result);
